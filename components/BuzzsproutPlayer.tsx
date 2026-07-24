19397ec4a6b368810231bf184a6b5119db2261b6
James Rhodes <vincerhodes@gmail.com>
2026-07-24 21:47:28 +0800

    feat: switch to Buzzsprout large smart player

--- components/BuzzsproutPlayer.tsx
+++ components/BuzzsproutPlayer.tsx
@@ -2,15 +2,15 @@ import Script from "next/script";
 import { site } from "@/site.config";
 
 /**
- * Official Buzzsprout small player embed for the show.
- * Renders the latest-episode player once the external script loads.
+ * Official Buzzsprout large smart player embed for the show.
+ * Renders the episode player once the external script loads.
  */
 export default function BuzzsproutPlayer() {
   return (
     <div className="rounded-lg border border-border bg-surface p-3">
-      <div id="buzzsprout-small-player" />
+      <div id="buzzsprout-large-player" />
       <Script
-        src={`https://www.buzzsprout.com/${site.buzzsproutId}.js?container_id=buzzsprout-small-player&player=small`}
+        src={`https://www.buzzsprout.com/${site.buzzsproutId}.js?container_id=buzzsprout-large-player&player=large`}
         strategy="lazyOnload"
         charSet="utf-8"
       />
